662aa34a805d32562a311c57d0f774b212f9b6ad
Momo <196245462+momoo038@users.noreply.github.com>
2025-05-26 17:56:48 +0200

    Finished home page

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,13 +1,55 @@
+import homeImage from "/src/img/home-img.jpg";
+
 function loadHome() {
   console.log("loadHome() executed."); // debug
 
   const content = document.querySelector("#content");
 
-  const content_h1 = document.createElement("h1");
-  content_h1.textContent = "This is the Home page!";
-  content_h1.classList.add("content-h1")
+  // Hero holder
+  const hero_div = document.createElement("div");
+  hero_div.classList.add("hero-div");
+  content.appendChild(hero_div);
+  // Hero holder
 
-  content.appendChild(content_h1)
+  // Image
+  const image_container = document.createElement("div");
+  image_container.classList.add("image");
+
+  const image = document.createElement("img");
+  image.src = homeImage;
+  image_container.appendChild(image);
+
+  hero_div.appendChild(image_container);
+  // Image
+
+  // Title
+  const title_container = document.createElement("div");
+  title_container.classList.add("title");
+
+  const title_1 = document.createElement("h1");
+  title_1.textContent = "Hungry?";
+  title_1.classList.add("title-home-1");
+  title_container.appendChild(title_1);
+
+  const title_2 = document.createElement("h1");
+  title_2.textContent = "Check out our menu!";
+  title_2.classList.add("title-home-2");
+  title_container.appendChild(title_2);
+
+  const title_para = document.createElement("p")
+  title_para.textContent = "Craving something delicious? Dive into our menu and discover a world of vibrant flavors crafted with passion and the freshest local ingredients. Your next favorite dish is just one click away!";
+  title_para.classList.add("title-para");
+  title_container.appendChild(title_para)
+
+  image_container.appendChild(title_container);
+  // Title
+
+  // Button
+  const button = document.createElement("button");
+  button.textContent = "MENU PAGE";
+  button.classList.add("btn");
+  title_container.appendChild(button);
+  // Button
 }
 
 export { loadHome };
