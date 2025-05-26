f981fe8d3851f3288840fcdf7c017918bc89227e
Momo <196245462+momoo038@users.noreply.github.com>
2025-05-26 15:19:43 +0200

    Explained the plan in short

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
+// about.js, home.js and menu.js export their respective functions and return homepage elements
+// index.js imports said functions, references main content div as well as the navigation
+// adds respective event listeners
+// executes loadHome on DOM load(?)
+
 import { loadHome } from "./home.js";
 import { loadMenu } from "./menu.js";
 import { loadAbout } from "./about.js";
